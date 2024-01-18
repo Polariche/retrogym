46ea3b5ad6b32b19390989ce21b0ea449306b8ef
Polariche <polariche@gmail.com>
2024-01-19 00:49:53 +0900

    reset if dead

--- src/example.py
+++ src/example.py
@@ -7,28 +7,35 @@ import random as rand
 def group_argb8888(data):
     return np.array(data).reshape(-1,4)[:,:3]
 
-e = libretro.Emulator()
-e.init("cores/2048/2048_libretro.so")
-
-# gently press the start button to start the game
-e.set_key(1 << 3)
-e.run()
-e.set_key(0)
-
-for i in range(60):
-
-    key = rand.randint(4,7)
-    
+def press_and_wait(key):
     e.set_key(1 << key)
     e.run()
     e.set_key(0)
     e.run()
+    e.run()
+    e.run()
+
+e = libretro.Emulator()
+e.init("cores/2048/2048_libretro.so")
+
+# gently press the start button to start the game
+data = e.get_memory_data(0)
+if (data[2] == 0):
+    press_and_wait(3)
+
+for i in range(120):
+
+    key = rand.randint(4,7)
+    press_and_wait(key)
     
     col = group_argb8888(e.get_video())
     data = e.get_memory_data(0)
 
-    print(data[0])
+    print(data[0], data[2])
 
     plt.imsave(f"screenshots/{i}.png", col.reshape(464,376,3))
+
+    if (data[2] == 2):
+        press_and_wait(3)
 
 e.deinit()
